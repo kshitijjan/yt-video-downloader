fd6473c19a80252cdaefcce13c30549af1de63cc
Kshitij Jain <kshitiz.jain636@gmail.com>
2025-03-09 23:59:13 +0530

    removed db

--- yt-backend/database/db.js
+++ yt-backend/database/db.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://admin:mongodb12@cluster0.9hqfpfp.mongodb.net/youtube',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then( () => console.log('Connected to MongoDB'))
-.catch( (err) => console.log('Could not connect to database', err));
+// mongoose.connect('mongodb+srv://admin:mongodb12@cluster0.9hqfpfp.mongodb.net/youtube',{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+// .then( () => console.log('Connected to MongoDB'))
+// .catch( (err) => console.log('Could not connect to database', err));
 
-const videoSchema = new mongoose.Schema({
-    url: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    title: String,
-    description: String,
-    thumbnail: String
-})
+// const videoSchema = new mongoose.Schema({
+//     url: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     title: String,
+//     description: String,
+//     thumbnail: String
+// })
 
-module.exports = mongoose.model('Video', videoSchema);
+// module.exports = mongoose.model('Video', videoSchema);
